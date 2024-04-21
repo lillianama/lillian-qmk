c24b8e219149c307c032f175600f7970617e5509
Lillian Mikus <lillianthesky@gmail.com>
2024-04-21 14:40:48 -0700

    enable one shot shift

--- keyboards/boardsource/unicorne/config.h
+++ keyboards/boardsource/unicorne/config.h
@@ -30,6 +30,8 @@
 #define ANALOG_JOYSTICK_X_AXIS_PIN GP27
 #define POINTING_DEVICE_INVERT_Y
 #define POINTING_DEVICE_INVERT_X
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
 // OLED DISPLAY
 #define OLED_FADE_OUT
